c17ecbcc32493f8949b1dece2a7b559b0ca45178
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-09-11 18:11:13 +0300

    define and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { User } from "./types";
+
 function App() {
+  const users: User[] = [
+    {
+      name: "Bikokwa Khaemba",
+      messages: [
+        {
+          body: "Prisma rocks!!",
+        },
+        {
+          body: "Did I mention I love Prisma?",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       <h2 className="text-4xl text-yellow-500">Hello World!</h2>
