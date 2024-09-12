23bc54c79489f74388d000b525dd6d0487ca66ae
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-09-12 06:54:31 +0300

    display user messages

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,4 +1,5 @@
 import { User } from "../types";
+import MessageDisplay from "./MessageDisplay";
 
 type Props = {
   user: User;
@@ -9,6 +10,11 @@ function UserDisplay({ user }: Props) {
     <div className="flex gap-x-24 justify-center">
       <div className="rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
         <p className="text-xl text-gray-200 font-bold">{user.name}</p>
+      </div>
+      <div>
+        {user.messages.map((message, i) => (
+          <MessageDisplay key={i} index={i} message={message} />
+        ))}
       </div>
     </div>
   );
